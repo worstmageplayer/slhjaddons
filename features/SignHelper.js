ee8e42dd86b4a2cd49635f288488fb0c37ec70f7
worstmageplayer <shawnjun05@gmail.com>
2025-05-06 11:11:39 +1000

    improved SignHelper

--- features/SignHelper.js
+++ features/SignHelper.js
@@ -1,43 +1,50 @@
 import Settings from "../config";
+import { RegisterGroup } from "../utils/RegisterStuff";
 import { calculator } from "./chatCalc";
 
 const GuiEditSign = Java.type("net.minecraft.client.gui.inventory.GuiEditSign");
 const tileSign = GuiEditSign.class.getDeclaredField("field_146848_f");
 tileSign.setAccessible(true);
-let lines = null;
-let result = null;
+let lines = result = display = null;
 
 const guiOpened = register('guiOpened', () => {
-    Client.scheduleTask(10, () => {
+    Client.scheduleTask(5, () => {
         const gui = Client.currentGui.get()
 
         if (!(gui instanceof GuiEditSign)) return;
-        const tile = tileSign.get(Client.currentGui.get());
+        const tile = tileSign.get(gui);
         lines = tile?.field_145915_a;
         if (!lines?.[0] || lines[1].func_150254_d().removeFormatting() !== "^^^^^^^^^^^^^^^") return;
 
-        guiRender.register();
-        guiClosed.register();
+        signHelper.register();
     })
 }).unregister()
 
-const guiRender = register('guiRender', (mx, my, gui) => {
-    const raw = lines[0].func_150254_d().removeFormatting().trim();
-    if (!raw) return;
-    result = calculator(raw)
-    const display = result == raw ? addCommas(result.toString()) : `${raw} = ${addCommas(result.toString())}`
+const signHelper = new RegisterGroup({
+    guiKey: register('guiKey', () => {
+        Client.scheduleTask(0, () => {
+            const raw = lines[0].func_150254_d().removeFormatting().trim();
+            if (!raw) {
+                display = null;
+                return;
+            }
+            result = calculator(raw)
+            display = result == raw ? addCommas(result.toString()) : `${raw} = ${addCommas(result.toString())}`
+        })
+    }).unregister(),
 
-    Renderer.drawString(display, (Renderer.screen.getWidth() - Renderer.getStringWidth(display)) / 2, 55, true);
-}).unregister()
+    guiRender: register('guiRender', () => {
+        if (!display) return;
+        Renderer.drawString(display, (Renderer.screen.getWidth() - Renderer.getStringWidth(display)) / 2, 55, true);
+    }).unregister(),
 
-const guiClosed = register('guiClosed', (gui) => {
-    if (!isNaN(result)) lines[0] = new net.minecraft.util.IChatComponent.Serializer().func_150699_a(`"${result}"`);
-
-    lines = null;
-    result = null;
-    guiRender.unregister();
-    guiClosed.unregister();
-}).unregister()
+    guiClosed: register('guiClosed', () => {
+        if (!isNaN(result)) lines[0] = new net.minecraft.util.IChatComponent.Serializer().func_150699_a(`"${result}"`);
+    
+        lines = result = display = null;
+        signHelper.unregister();
+    }).unregister()
+})
 
 Settings.registerListener("Sign Helper", v => v ? guiOpened.register() : guiOpened.unregister());
 if (Settings.toggleSignHelper) guiOpened.register();
