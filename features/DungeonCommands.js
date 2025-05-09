fb54d9a8c38003092fa2e5eca6d403822d2596d8
worstmageplayer <shawnjun05@gmail.com>
2025-05-10 03:16:13 +1000

    DungeonCommands stuff

--- features/DungeonCommands.js
+++ features/DungeonCommands.js
@@ -8,41 +8,33 @@ let addedToQueue = false;
 
 const dungeonCommands = new RegisterGroup({
     command: register('command', (arg) => {
-        if (!arg) {
-            ChatLib.command('warp dungeons');
-            return;
-        }
+        if (!arg) return ChatLib.command('warp dungeons');
     
-        let match = arg.toLowerCase().match(/^(m|f)(\d)$/);
+        const match = arg.toLowerCase().match(/^(m|f)(\d)$/);
         if (!match) return;
     
-        const [daWholeTing, prefix, num] = match;
+        const [_, prefix, num] = match;
         const floorNum = parseInt(num);
+        if (!['m', 'f'].includes(prefix) || floorNum < 1 || floorNum > 7) return;
     
-        if ((prefix === 'm' || prefix === 'f') && floorNum >= 1 && floorNum <= 7) {
-            const floorWord = floorName[floorNum - 1];
-            const instanceType = prefix === 'm' ? "MASTER_CATACOMBS" : "CATACOMBS";
+        const floorWord = floorName[floorNum - 1];
+        const instanceType = prefix === 'm' ? "MASTER_CATACOMBS" : "CATACOMBS";
     
-            let timeStart = Date.now() - timerStart;
-            let timeLeft = Math.max(0, 30000 - timeStart);
+        const timeStart = Date.now() - timerStart;
+        const timeLeft = Math.max(0, 30000 - timeStart);
     
-            if (timeLeft > 0) {
-                if (addedToQueue) {
-                    ChatLib.chat(`Rejoining in ${(timeLeft / 1000).toFixed(2)}s`)
-                    return;
-                };
-                addedToQueue = true;
-                ChatLib.command(`pc Waiting for cooldown, rejoining in ${(timeLeft / 1000).toFixed(2)}s.`);
-            }
-    
-            setTimeout(() => {
-                ChatLib.command(`joininstance ${instanceType}_FLOOR_${floorWord}`);
-                enterUndersized();
-                addedToQueue = false;
-            }, timeLeft + 100);
-    
-            return;
+        if (timeLeft > 0) {
+            if (addedToQueue) return ChatLib.chat(`Rejoining in ${(timeLeft / 1000).toFixed(2)}s`);
+            addedToQueue = true;
+            ChatLib.command(`pc Waiting for cooldown, rejoining in ${(timeLeft / 1000).toFixed(2)}s.`);
         }
+    
+        setTimeout(() => {
+            ChatLib.command(`joininstance ${instanceType}_FLOOR_${floorWord}`);
+            enterUndersized();
+            addedToQueue = false;
+        }, timeLeft + 100);
+        
     }).setName('d').unregister(),
 
     chat: register('chat', (rank, name, dungeontype, number) => {
