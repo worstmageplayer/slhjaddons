f72425cbe1ae574758b4a85d3b365335d217c3c1
worstmageplayer <shawnjun05@gmail.com>
2025-05-11 01:52:07 +1000

    fised blood timer

--- features/BloodWarpTimer.js
+++ features/BloodWarpTimer.js
@@ -3,23 +3,26 @@ import Settings from "../config";
 import { WorldInfo } from "../utils/WorldInfo";
 import { RegisterGroup } from "../utils/RegisterStuff";
 
-const worldInfo = new WorldInfo
+const worldInfo = new WorldInfo()
 
 // Enter blood at 10s
 let bloodOpen = null;
-const bloodWarpTimer = new RegisterGroup({
-    chat: register('chat', () => {
-        if (!worldInfo.inDungeon()) return;
-        bloodOpen = Date.now();
-    }).setCriteria('The BLOOD DOOR has been opened!').setParameter('contains').unregister(),
 
+const bloodWarpTimer = register('chat', () => {
+    if (!worldInfo.inDungeon()) return;
+    bloodOpen = Date.now();
+    blood.register();
+}).setCriteria('The BLOOD DOOR has been opened!').setParameter('contains').unregister();
+
+const blood = new RegisterGroup({
     renderOverlay: register('renderOverlay', () => {
         if (!bloodOpen) return;
         const time = ((Date.now() - bloodOpen) / 1000).toFixed(1);
         let colorCode = time < 5 ? 'a' : time < 7.5 ? 'e' : 'c';
         
-        if (time > 11) {
+        if (time > 20) {
             bloodOpen = null;
+            blood.unregister();
             return;
         }
         Renderer.drawString(
@@ -30,7 +33,10 @@ const bloodWarpTimer = new RegisterGroup({
         );
     }).unregister(),
 
-    worldUnload: register('worldUnload', () => bloodOpen = null).unregister()
+    worldUnload: register('worldUnload', () => {
+        bloodOpen = null; 
+        blood.unregister();
+    }).unregister()
 })
 
 Settings.registerListener('Blood Time', v => v ? bloodWarpTimer.register() : bloodWarpTimer.unregister());
