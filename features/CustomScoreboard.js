2a77d88d20753cc2ead958110901c70f7e0b4d19
worstmageplayer <shawnjun05@gmail.com>
2025-05-13 10:52:41 +1000

    scoreboard changes

--- features/CustomScoreboard.js
+++ features/CustomScoreboard.js
@@ -4,12 +4,6 @@ import { RegisterGroup } from '../utils/RegisterStuff';
 
 let scoreboardText = null;
 
-let scoreboardScale = Settings.scoreboardScale;
-let scoreboardShadow = Settings.toggleScoreboardShadow;
-let scoreboardPadding = Settings.scoreboardPadding;
-let scoreboardOffset = Settings.scoreboardOffset;
-let scoreboardColour = Settings.scoreboardColour.getRGB();
-
 let xPos = 0, yPos = 0;
 let width = 0, height = 0;
 let bgxPos = 0, bgyPos = 0, bgWidth = 0, bgHeight = 0;
@@ -21,28 +15,20 @@ const scoreboard = new RegisterGroup({
     renderScoreboard: register('renderScoreboard', (event) => cancel(event)).unregister(),
     renderOverlay: register('renderOverlay', () => {
         if (!scoreboardText) return;
-    
-        Renderer.scale(scoreboardScale);
-        Renderer.drawRect(scoreboardColour, bgxPos, bgyPos, bgWidth, bgHeight);
-        Renderer.scale(scoreboardScale);
-        Renderer.drawString(scoreboardText, xPos, yPos, scoreboardShadow);
+        Renderer.retainTransforms(true);
+        Renderer.scale(Settings.scoreboardScale);
+        Renderer.drawRect(Settings.scoreboardColour.getRGB(), bgxPos, bgyPos, bgWidth, bgHeight);
+        Renderer.drawString(scoreboardText, xPos, yPos, Settings.toggleScoreboardShadow);
+        Renderer.retainTransforms(false);
     }).unregister(),
     packetReceived: register('packetReceived', () => updateScoreboardLines()).setFilteredClass(S3EPacketTeams).unregister(),
     worldLoad: register('worldLoad', () => Client.scheduleTask(10, () => updateScoreboardLines())).unregister(),
     worldUnload: register('worldUnload', () => scoreboardText = '').unregister(),
 })
 
-// Scoreboard Render update
-Settings.registerListener('Scoreboard Scale ', value => scoreboardScale = value)
-Settings.registerListener("Scoreboard Shadow", value => scoreboardShadow = value)
-Settings.registerListener("Scoreboard Padding", value => scoreboardPadding = value)
-Settings.registerListener("Scoreboard Offset", value => scoreboardOffset = value)
-Settings.registerListener("Scoreboard Colour", value => scoreboardColour = value.getRGB())
 Settings.registerListener('Custom Scoreboard', v => v ? scoreboard.register() : scoreboard.unregister());
 
-if (Settings.toggleCustomScoreboard) {
-    scoreboard.register();
-}
+if (Settings.toggleCustomScoreboard) scoreboard.register();
 
 function updateScoreboardLines() {
     const scoreboardLines = Scoreboard.getLines();
@@ -74,10 +60,10 @@ function updateScoreboardPosition() {
     const screenWidth = Renderer.screen.getWidth() * rendererScale / guiScale;
     const screenHeight = Renderer.screen.getHeight() * rendererScale / guiScale;
     
-    xPos = (screenWidth - scoreboardOffset) / scoreboardScale - width;
-    yPos = (screenHeight / scoreboardScale - height) / 2;
-    bgxPos = xPos - scoreboardPadding;
-    bgyPos = yPos - scoreboardPadding;
-    bgWidth = width + 2 * scoreboardPadding + scoreboardOffset;
-    bgHeight = height + 2 * scoreboardPadding;
+    xPos = (screenWidth - Settings.scoreboardOffset) / Settings.scoreboardScale - width;
+    yPos = (screenHeight / Settings.scoreboardScale - height) / 2;
+    bgxPos = xPos - Settings.scoreboardPadding;
+    bgyPos = yPos - Settings.scoreboardPadding;
+    bgWidth = width + 2 * Settings.scoreboardPadding + Settings.scoreboardOffset;
+    bgHeight = height + 2 * Settings.scoreboardPadding;
 }
