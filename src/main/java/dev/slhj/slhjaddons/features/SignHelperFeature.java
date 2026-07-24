dd454de69f957e0c4831f0da35c89070c536fdbf
worstmageplayer <shawnjun05@gmail.com>
2026-07-24 14:44:12 +1000

    Sign Slider

--- src/main/java/dev/slhj/slhjaddons/features/SignHelperFeature.java
+++ src/main/java/dev/slhj/slhjaddons/features/SignHelperFeature.java
@@ -2,6 +2,7 @@ package dev.slhj.slhjaddons.features;
 
 import dev.slhj.slhjaddons.calc.Calculator;
 import dev.slhj.slhjaddons.core.Feature;
+import dev.slhj.slhjaddons.core.Setting;
 import dev.slhj.slhjaddons.util.RenderUtils;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
@@ -11,9 +12,12 @@ public final class SignHelperFeature extends Feature {
 
     private String displayFormula = null;
 
+    private final Setting.SliderSetting yPos;
+
     public SignHelperFeature() {
         setLabel("Sign Helper");
         category(Category.SKYBLOCK);
+        yPos = slider("sign_helper.ypos", "Y-Pos", 0.0, 100, 55);
     }
 
     @Override public String id() { return "sign_helper"; }
@@ -26,7 +30,7 @@ public final class SignHelperFeature extends Feature {
 
         int screenWidth = g.guiWidth();
         int x = (screenWidth - RenderUtils.stringWidth(displayFormula)) / 2;
-        RenderUtils.text(g, displayFormula, x, 55, 0xFFFFFFFF, true);
+        RenderUtils.text(g, displayFormula, x, yPos.value().get().intValue(), 0xFFFFFFFF, true);
     }
 
     public void setDisplayFormula(String formula) {
