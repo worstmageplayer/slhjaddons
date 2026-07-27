38efecb0e0bd9fae00605a0a4a29b568d4f473a4
worstmageplayer <shawnjun05@gmail.com>
2026-07-27 21:18:48 +1000

    rename

--- src/main/java/dev/slhj/slhjaddons/mixin/GameRendererMixin.java
+++ src/main/java/dev/slhj/slhjaddons/mixin/GameRendererMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
-public abstract class HurtMixin {
+public abstract class GameRendererMixin {
     @Inject(method = "bobHurt", at = @At("HEAD"), cancellable = true)
     private void slhj$bobHurt(CameraRenderState cameraState, PoseStack poseStack, CallbackInfo ci) {
         if (SlhjAddons.config().isFeatureEnabled(NoBobHurt.id)) ci.cancel();
